922c92ddc90e6d20b777d52f0c68d1fee708c367
Titouan Herbaut <titouan.herbaut46@gmail.com>
2026-02-05 11:21:02 +0100

    TP10 : exercice 5 debut

--- TP10/src/GroupeAgenda.java
+++ TP10/src/GroupeAgenda.java
@@ -1,2 +1,32 @@
-public class GroupeAgenda {
+public class GroupeAgenda extends AgendaAbstrait {
+
+    public GroupeAgenda(String nom) {
+        super(nom);
+    }
+
+    public void ajouter(Agenda agenda) {
+    }
+
+    @Override
+    public String getNom() {
+        return "";
+    }
+
+    @Override
+    public void enregistrer(int creneau, String rdv) throws OccupeException {
+
+    }
+
+    @Override
+    public boolean annuler(int creneau) {
+        return false;
+    }
+
+    @Override
+    public String getRendezVous(int creneau) throws LibreException {
+        return "";
+    }
+
+    public void proposer(int i, String ok) {
+    }
 }
