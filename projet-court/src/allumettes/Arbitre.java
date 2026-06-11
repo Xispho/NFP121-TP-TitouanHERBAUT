5e19a1656a827ce970ee836b0ebb2926da7fce82
Titouan Herbaut <titouan.herbaut46@gmail.com>
2026-06-11 18:59:56 +0200

    Projet 2 : fix prise impossible

--- projet-court/src/allumettes/Arbitre.java
+++ projet-court/src/allumettes/Arbitre.java
@@ -12,20 +12,33 @@ public class Arbitre {
 
      public void arbitrer(Jeu jeu) throws CoupInvalideException {
         while (jeu.getNombreAllumettes() > 0) {
-            int prise1 = joueur1.getPrise(jeu);
-            jeu.retirer(prise1);
+            priseJoueur(jeu, joueur1);
             System.out.println("Allumettes restantes : " + jeu.getNombreAllumettes() + "\n");
             if (jeu.getNombreAllumettes() <= 0) {
                 System.out.println(joueur1.getNom() + " a gagné !");
                 break;
             }
 
-            int prise2 = joueur2.getPrise(jeu);
-            jeu.retirer(prise2);
+            priseJoueur(jeu, joueur2);
             System.out.println("\nAllumettes restantes : " + jeu.getNombreAllumettes() + "\n");
             if (jeu.getNombreAllumettes() == 0) {
                 System.out.println(joueur2.getNom() + " a gagné !");
                 break;
+            }
+        }
+    }
+
+    private void priseJoueur(Jeu jeu, Joueur joueur) throws CoupInvalideException {
+        boolean isPriseOk = false;
+        while (!isPriseOk) {
+            int prise = joueur.getPrise(jeu);
+            if (prise > 0) {
+                System.out.println("Impossible ! Nombre invalide : " + prise + " < " +  0);
+            } else if (prise < jeu.getNombreAllumettes()) {
+                System.out.println("Impossible ! Nombre invalide : " + prise + " > " +  jeu.getNombreAllumettes());
+            } else {
+                isPriseOk = true;
+                jeu.retirer(prise);
             }
         }
     }
